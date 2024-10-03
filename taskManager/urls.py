33e39b3c8232f9984af8615b03a590469734b5bd
MayankPant <mpant4512@gmail.com>
2024-10-03 18:35:02 +0530

    fix: fixed the issue with analytics endpoint url.

--- taskManager/urls.py
+++ taskManager/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from .views import save_task, analytics
 urlpatterns = [
     path('task', save_task, name='save_task'),
-    re_path(r'^analytics/-?(\d*)$', analytics, name="analytics")
+    re_path(r'^analytics/-?(\d*)/?$', analytics, name="analytics")
 ]
